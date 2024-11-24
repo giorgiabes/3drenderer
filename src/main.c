53ed26d50760e1a9853839af576b34aa15baabc6
giorgiabes <abesadze.george@gmail.com>
2024-11-24 21:24:13 +0400

    302

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_render.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
@@ -35,9 +38,49 @@ bool initialize_window(void) {
 	return true;
 }
 
-int main(void) {
+void setup(void) {
+	// TODO:
+}
 
+void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+		case SDL_QUIT: {
+			is_running = false;
+			break;
+		}
+		case SDL_KEYDOWN: {
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				is_running = false;
+				break;
+			}
+		}
+	}
+}
+
+void update(void) {
+	// TODO:
+}
+
+void render(void) {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+}
+
+int main(void) {
 	is_running = initialize_window();
+
+	setup();
+
+	while (is_running) {
+		process_input();
+		update();
+		render();
+	}
 
 	return 0;
 }
