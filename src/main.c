b7fad0b0ad827b1a389e79de87335a1fe3223604
giorgiabes <abesadze.george@gmail.com>
2024-11-27 14:24:27 +0400

    503

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
 #include "display.h"
+#include "vector.h"
 
 bool is_running = false;
 
@@ -59,6 +59,8 @@ void render(void) {
 int main(void) {
         is_running = initialize_window();
         setup();
+
+        vec3_t myvector = {2.0, 3.0, -4.0};
 
         while (is_running) {
                 process_input();
