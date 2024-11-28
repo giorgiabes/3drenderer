472d9fdff06076a6c5d5a32fd1ce404d5e3c90da
giorgiabes <abesadze.george@gmail.com>
2024-11-28 23:30:01 +0400

    end 8

--- src/display.c
+++ src/display.c
@@ -2,25 +2,24 @@
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
-
 uint32_t *color_buffer = NULL;
 SDL_Texture *color_buffer_texture = NULL;
-
 int window_width = 800;
 int window_height = 600;
 
 bool initialize_window(void) {
-
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     fprintf(stderr, "Error initializing SDL.\n");
     return false;
   }
 
+  // Set width and height of the SDL window with the max screen resolution
   SDL_DisplayMode display_mode;
   SDL_GetCurrentDisplayMode(0, &display_mode);
   window_width = display_mode.w;
   window_height = display_mode.h;
 
+  // Create a SDL Window
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                        window_width, window_height, SDL_WINDOW_BORDERLESS);
@@ -29,21 +28,20 @@ bool initialize_window(void) {
     return false;
   }
 
+  // Create a SDL renderer
   renderer = SDL_CreateRenderer(window, -1, 0);
   if (!renderer) {
     fprintf(stderr, "Error creating SDL renderer.\n");
     return false;
   }
 
-  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
   return true;
 }
 
-void draw_grid() {
-  for (int y = 0; y < window_height; y += 50) {
-    for (int x = 0; x < window_width; x += 50) {
-      color_buffer[(window_width * y) + x] = 0xFFFF00FF;
+void draw_grid(void) {
+  for (int y = 0; y < window_height; y += 10) {
+    for (int x = 0; x < window_width; x += 10) {
+      color_buffer[(window_width * y) + x] = 0xFF444444;
     }
   }
 }
@@ -67,7 +65,6 @@ void draw_rect(int x, int y, int width, int height, uint32_t color) {
 void render_color_buffer(void) {
   SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer,
                     (int)(window_width * sizeof(uint32_t)));
-
   SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
 }
 
