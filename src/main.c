225f92d83a7206db1c051dfa5b0edcee5ade10b4
giorgiabes <abesadze.george@gmail.com>
2024-12-12 12:42:25 +0400

    0301\n

--- src/main.c
+++ src/main.c
@@ -1,14 +1,38 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
+#include <stdbool.h>
 #include <stdio.h>
 
-void initialize_window(void) {
+bool is_running = false;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     fprintf(stderr, "Error initializing SDL.\n");
+    return false;
   }
+  // Create a SDL window
+  window =
+      SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       800, 600, SDL_WINDOW_BORDERLESS);
+  if (!window) {
+    fprintf(stderr, "Error creating SDL window.\n");
+    return false;
+  }
+
+  // Create a SDL renderer
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer) {
+    fprintf(stderr, "Error creating SDL renderer.\n");
+    return false;
+  }
+
+  return true;
 }
 
 int main(void) {
-  initialize_window();
+  is_running = initialize_window();
 
   return 0;
 }
