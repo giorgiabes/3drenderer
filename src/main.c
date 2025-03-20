a37d7c68e0904fab8a0075dfabf2471df8ec8bfe
giorgiabes <abesadze.george@gmail.com>
2025-03-20 21:41:03 +0400

    allocationg memory and freeing resources

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <stdlib.h>
 
 bool is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -17,8 +23,8 @@ bool initialize_window(void) {
     NULL,
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
-    800,
-    600,
+    window_width,
+    window_height,
     SDL_WINDOW_BORDERLESS
   );
   if (!window) {
@@ -36,7 +42,7 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
-  // TODO:
+  color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 }
 
 void process_input(void) {
@@ -57,11 +63,23 @@ void process_input(void) {
 void update() {
   // TODO:
 }
+
+void clear_color_buffer(void) {}
+
 void render() {
   SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
   SDL_RenderClear(renderer);
 
+  clear_color_buffer();
+
   SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void) {
+  free(color_buffer);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
 
 int main(void) {
@@ -75,6 +93,8 @@ int main(void) {
     update();
     render();
   }
+
+  destroy_window();
 
   return 0;
 }
